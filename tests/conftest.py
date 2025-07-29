8e53babf3215ebad925ff6a84f1410fa6de04738
Avi <avi.vajpeyi@gmail.com>
2025-07-29 16:04:23 +1200

    add pyproj commands

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
 import os
-
+import contextlib
 import pytest
 from unittest.mock import patch
-
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,8 +15,16 @@ def outdir():
     return outdir
 
 
-
 @pytest.fixture
 def mock_data_dir():
     with patch('starccato_lvk.data_acquisition.config.DATA_DIR', new=TEST_DIR):
         yield
+
+
+@pytest.fixture
+def mock_get_data_files_and_gps_times(monkeypatch):
+    def fake_func(*args, **kwargs):
+        return {
+            i: f"file_{i}.hdf5" for i in range(100, 200, 10)
+        }
+    monkeypatch.setattr('starccato_lvk.data_acquisition.io.utils._get_data_files_and_gps_times', fake_func)
